6435c81d3a1e7e2997aacfb1d0bfec233bd6b0c1
Silva Bouchard, Jesus Alejandro <jasilva.22@est.ucab.edu.ve>
2024-07-11 22:59:48 -0400

    Update ListaJornadasAnteriores.tsx

--- App/FrontEnd/src/Paginas/Estadisticas/ListaDeJornadas/ListaJornadasAnteriores.tsx
+++ App/FrontEnd/src/Paginas/Estadisticas/ListaDeJornadas/ListaJornadasAnteriores.tsx
@@ -1,6 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import BotonLista from "Componentes/BotonLista/BotonLista";
 import styles from "./ListaJornadasAnteriores.module.css";
+import { useState } from "react";
 
 interface props {
   jornadas: {
@@ -8,6 +9,7 @@ interface props {
     nombre: string;
     fecha: string;
   }[];
+  onClick?: (number) => void;
 }
 
 const lista: React.FC<props> = (props) => {
@@ -18,21 +20,54 @@ const lista: React.FC<props> = (props) => {
           key={jornada.id}
           titulo={jornada.nombre}
           subtitulo={jornada.fecha}
-          onClick={() => console.log(jornada.id)}></BotonLista>
+          onClick={() =>
+            props.onClick && props.onClick(jornada.id)
+          }></BotonLista>
       ))}
     </Stack>
   );
 };
 
+interface propsEstadisticasJornadaEspesifica {
+  id: number;
+}
+
+const EstadisticasJornadaEspesifica: React.FC<
+  propsEstadisticasJornadaEspesifica
+> = ({ id }) => {
+  return (
+    <div>
+      <Typography variant="h4"> Encuestas {id} </Typography>
+      <Typography variant="h6"> Estadistica de Dona </Typography>
+      {/* EstadisticaDona */}
+      <Typography variant="h6"> Estadistica de Barra </Typography>
+      {/* EstadisticaBarra */}
+    </div>
+  );
+};
+
 const ListaJornadasAnteriores: React.FC = () => {
+  const [jornadaSeleccionada, setJorandaSeleccionada] = useState(-1);
+
   return (
     <div className={styles.all}>
-      <div className={styles.title}>
-        <Typography variant="h4">Lista de Jornadas</Typography>
-      </div>
-      {lista({
-        jornadas: [{ id: 1, nombre: "Jornada 1", fecha: "01/01/2021" }],
-      })}
+      {jornadaSeleccionada !== -1 ? (
+        <EstadisticasJornadaEspesifica id={jornadaSeleccionada} />
+      ) : (
+        <>
+          <div className={styles.title}>
+            <Typography variant="h4">Lista de Jornadas</Typography>
+          </div>
+          {lista({
+            jornadas: [
+              { id: 1, nombre: "Jornada 1", fecha: "01/01/2021" },
+              { id: 2, nombre: "Jornada 2", fecha: "01/01/2021" },
+              { id: 3, nombre: "Jornada 3", fecha: "01/01/2021" },
+            ],
+            onClick: (id) => setJorandaSeleccionada(id),
+          })}
+        </>
+      )}
     </div>
   );
 };
